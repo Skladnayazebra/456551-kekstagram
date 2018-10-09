7c6675f782e0fb82fde18a7549bedb9dddf1b932
Алексей Михайленко <skladnayazebra@gmail.com>
2018-10-09 23:26:42 +1000

    обработчик отправки формы переехал в upload-window.js

--- js/upload-window.js
+++ js/upload-window.js
@@ -6,6 +6,11 @@
   var imgUploadOverlayCloseBtn = document.querySelector('.img-upload__cancel');
   var inputHashtags = document.querySelector('.text__hashtags');
   var inputDescription = document.querySelector('.text__description');
+  var form = document.querySelector('.img-upload__form');
+  var successDialogTemplate = document.querySelector('#success').content.querySelector('.success');
+  var successButton = document.querySelector('.success__button')
+  var failDialogTemplate = document.querySelector('#error').content.querySelector('.error');
+  var body = document.querySelector('body');
 
   var onUploadOverlaySetEffect = function () {
     window.effects.applyEffect(window.effects.EFFECT_LEVEL_DEFAULT);
@@ -17,6 +22,19 @@
     window.effects.applyEffect(window.effects.EFFECT_LEVEL_DEFAULT);
     inputHashtags.value = null;
     inputDescription.value = null;
+  };
+
+  var onSuccess = function () {
+    window.util.hideElement(imgUploadOverlay);
+    var successMessage = successDialogTemplate.cloneNode(true);
+    body.appendChild(successMessage);
+
+
+  };
+
+  var onFail = function () {
+    var failMessage = failDialogTemplate.cloneNode(true);
+    body.appendChild(failMessage);
   };
 
   imgUploadField.addEventListener('change', function () {
@@ -37,4 +55,10 @@
     window.util.onEnterPressClose(evt, imgUploadOverlay);
     onUploaderOverlayClean();
   });
+
+  form.addEventListener('submit', function (evt) {
+    window.upload(new FormData(form), onSuccess, onFail);
+    evt.preventDefault();
+  });
+
 })();
