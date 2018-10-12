9e6e35d99aac9f536abcc8f86fb2e38f0adf0338
Алексей Михайленко <skladnayazebra@gmail.com>
2018-10-12 21:20:27 +1000

    Код из блока scale переехал в блок effects

--- js/effects.js
+++ js/effects.js
@@ -5,7 +5,37 @@
   // 1) сделать единую функцию для применения эффекта,
   // 2) вместо отдельных обработчков использовать общий с делегированием.
   var EFFECT_LEVEL_DEFAULT = 100;
+  var SCALE_MAX = 100;
+  var SCALE_MIN = 25;
+  var SCALE_STEP = 25;
+  var SCALE_DEFAULT = 100;
+
   var imgPreview = document.querySelector('.img-upload__preview img');
+
+  var scaleControlSmaller = document.querySelector('.scale__control--smaller');
+  var scaleControlBigger = document.querySelector('.scale__control--bigger');
+  var scaleControlField = document.querySelector('.scale__control--value');
+
+  var currentScale = SCALE_DEFAULT;
+
+  var decreaseSize = function () {
+    if (currentScale > SCALE_MIN) {
+      scaleControlField.value = (currentScale - SCALE_STEP) + '%';
+      imgPreview.style.transform = 'scale(' + (currentScale - SCALE_STEP) / 100 + ')';
+      currentScale -= SCALE_STEP;
+    }
+  };
+
+  var increaseSize = function () {
+    if (currentScale < SCALE_MAX) {
+      scaleControlField.value = (currentScale + SCALE_STEP) + '%';
+      imgPreview.style.transform = 'scale(' + (currentScale + SCALE_STEP) / 100 + ')';
+      currentScale += SCALE_STEP;
+    }
+  };
+
+  scaleControlSmaller.addEventListener('click', decreaseSize);
+  scaleControlBigger.addEventListener('click', increaseSize);
 
   var effectLevelLine = document.querySelector('.effect-level__line');
   var effectLevelField = document.querySelector('.img-upload__effect-level');
